fc77e50ce094ce7197b641f05b85626859d6bde9
Andrii Turko <aturko@vector-software.com>
2024-06-16 12:07:02 +0300

    add task_3

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <thread>
+#include <mutex>
 
 int counter = 0;
+std::mutex m;
 
 int get_id() {
     std::hash<std::thread::id> hasher;
@@ -11,18 +13,24 @@ int get_id() {
 
 int main() {
     std::thread t1([]() {
+        m.lock();
         counter += get_id();
         std::cout << "Thread 1 counter: " << counter << std::endl;
+        m.unlock();
     });
 
     std::thread t2([]() {
+        m.lock();
         counter += get_id();
         std::cout << "Thread 2 counter: " << counter << std::endl;
+        m.unlock();
     });
 
     std::thread t3([]() {
+        m.lock();
         counter += get_id();
         std::cout << "Thread 3 counter: " << counter << std::endl;
+        m.unlock();
     });
 
     t1.join();
